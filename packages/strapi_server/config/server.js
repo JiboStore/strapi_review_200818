aa240abec5766234fa5ecf823b441d08864357e7
Hakim Hauston <hakim.hauston@storms.com>
2020-08-18 23:25:50 +0800

    [strapi] npx create-strapi-app strapi_server

---
+++ packages/strapi_server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd7af55bfbe633924429983d91a3e3fe7'),
+    },
+  },
+});
